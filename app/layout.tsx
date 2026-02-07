2a6259966a06e2b5b22a9f928a3a091c6e08cf40
yohanZiard <75817970+yohanZiard@users.noreply.github.com>
2026-02-07 19:57:30 +0530

    Update layout.tsx
    as this guy is telling me

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
- import { GoogleOAuthProvider } from "@react-oauth/google"
+import Providers from "./providers"
+import { GoogleOAuthProvider } from "@react-oauth/google"
 import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
